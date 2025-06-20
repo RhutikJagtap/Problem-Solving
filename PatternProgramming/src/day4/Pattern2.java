42cbe87047c286c69685390aa8cdf0227754360c
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-06-20 20:22:39 +0530

    right angle triangle

--- PatternProgramming/src/day4/Pattern2.java
+++ PatternProgramming/src/day4/Pattern2.java
@@ -1,17 +1,16 @@
 package day4;
 
-public class Pattern1 {
-
+public class Pattern2 {
+	
 	public static void main(String[] args) {
 		for(int i=1;i<=5;i++)
 		{
 			for(int j=1;j<=i;j++)
 			{
-				System.out.print("*"+" ");
+				System.out.print(j+" ");
 			}
 			System.out.println();
 		}
-
 	}
 
 }
--- PatternProgramming/src/day4/Pattern3.java
+++ PatternProgramming/src/day4/Pattern3.java
@@ -1,17 +1,15 @@
 package day4;
 
-public class Pattern1 {
-
+public class Pattern3 {
 	public static void main(String[] args) {
 		for(int i=1;i<=5;i++)
 		{
 			for(int j=1;j<=i;j++)
 			{
-				System.out.print("*"+" ");
+				System.out.print(i+" ");
 			}
 			System.out.println();
 		}
-
 	}
 
 }
