679dc586c5b10540da00f4cb0949461a43fe29e4
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-06-22 11:02:19 +0530

    day6 3rd pattern

---
+++ PatternProgramming/src/day6/Pattern3.java
@@ -0,0 +1,19 @@
+package day6;
+
+public class Pattern3 {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++)
+		{
+			for(int spaces=1;spaces<=5-i;spaces++)
+			{
+				System.out.print("  ");
+			}
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(j+"  ");
+			}
+			System.out.println();
+		}
+	}
+
+}
