f3ec2e5917db45aafdbf1c735675689f5e930593
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-06-22 11:16:34 +0530

    gcd program brute force approach

---
+++ DSA/src/day3/GCD.java
@@ -0,0 +1,40 @@
+package day3;
+
+import java.util.Scanner;
+
+public class GCD{
+	
+	static int gcd(int n1,int n2)
+	{
+		int min=0;
+		if(n1<n2)
+		{
+			min=n1;
+		}else {
+			min=n2;
+		}
+		
+		for(int i=min;i>=1;i--)
+		{
+			if (n1%i==0 && n2%i==0) {
+				return i;
+			}
+		}
+		
+		return 1;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter the Number first");
+		int n1 = scanner.nextInt();
+		
+		System.out.println("Enter the Number second");
+		int n2 = scanner.nextInt();
+		
+		int gcd = gcd(n1,n2);
+		System.out.println("GCD = "+gcd);
+	}
+
+}
