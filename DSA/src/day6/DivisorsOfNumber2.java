b12da447518677b1ca277bd4e811761915b7aa3a
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-06-26 09:40:44 +0530

    efficent to print divisors of number

---
+++ DSA/src/day6/DivisorsOfNumber2.java
@@ -0,0 +1,27 @@
+package day6;
+
+import java.util.Scanner;
+
+public class DivisorsOfNumber2 {
+	
+	static void printDivisors(int n)
+	{
+		for(int i=1;i*i<=n;i++)
+		{
+				if (n%i==0) {
+					System.out.println(i+"  "+n/i);											
+				}
+		
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter a Number");
+		int n = scanner.nextInt();
+		
+		printDivisors(n);
+	}
+
+}
