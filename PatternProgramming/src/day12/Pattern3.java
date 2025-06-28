fa4ac0da8dde3f835ef0a5abfac9e0fa96bf6ed2
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-06-28 19:23:36 +0530

    combination of number and star

---
+++ PatternProgramming/src/day12/Pattern3.java
@@ -0,0 +1,19 @@
+package day12;
+
+public class Pattern3 {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++)
+		{
+			int count=i*(i+1)/2;
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(count--);
+				if (j<i) {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
