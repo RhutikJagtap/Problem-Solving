4db57998a084819272dfa57fc30432b27f98a2ab
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-06-28 19:19:31 +0530

    sum of first row natural number

---
+++ PatternProgramming/src/day12/Pattern2.java
@@ -0,0 +1,17 @@
+package day12;
+
+public class Pattern2 {
+	
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++)
+		{
+			int count=i*(i+1)/2;
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(count-- +" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
