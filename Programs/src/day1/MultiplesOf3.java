f79647c495b9bf10b7bb99c030231cc203b3b378
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-06-30 22:17:23 +0530

    multiples of 3

---
+++ Programs/src/day1/MultiplesOf3.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class MultiplesOf3 {
+	
+	public static void main(String[] args) {
+		for(int i=1;i<=20;i++)
+		{
+			if (i%3==0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
