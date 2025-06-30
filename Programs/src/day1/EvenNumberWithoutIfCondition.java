87934a4794ffc31ea6ac19c9a1c02eea55bc82b5
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-06-30 22:26:08 +0530

    even numbers without using if condition

---
+++ Programs/src/day1/EvenNumberWithoutIfCondition.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class EvenNumberWithoutIfCondition {
+	
+	public static void main(String[] args) {
+		for (int i = 2; i <=20; i=i+2) {
+			System.out.println(i);
+		}
+	}
+
+}
