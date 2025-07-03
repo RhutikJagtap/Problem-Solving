ddf2c26f27aeda339ca28d462cafe38eff32c975
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-03 17:17:34 +0530

    round of up to 2 decimal palces

---
+++ Programs/src/day4/RoundOf.java
@@ -0,0 +1,19 @@
+package day4;
+
+public class RoundOf {
+	
+	public static void main(String[] args) {
+		float x=323.648388337f;
+		double y=123.345678;
+		
+		System.out.println(x);
+		System.out.printf("%.2f",x);
+		System.out.println();
+		
+		System.out.println("==============");
+		
+		System.out.println(y);
+		System.out.printf("%.2f",y);
+	}
+
+}
