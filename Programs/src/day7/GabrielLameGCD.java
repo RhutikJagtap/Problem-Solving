7a474a9aa7dc8c3eb3fccb136d42db1c76973695
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-07 14:33:26 +0530

    gabriel lame gcd

---
+++ Programs/src/day7/GabrielLameGCD.java
@@ -0,0 +1,37 @@
+package day7;
+
+import java.util.Scanner;
+
+public class GabrielLameGCD {
+	
+	static int lameGCD(int n1,int n2)
+	{
+		while (n1!=0 && n2!=0) {
+			if (n1>n2) {
+				n1=n1%n2;
+			} else {
+				n2=n2%n1;
+			}
+		}
+		
+		if (n1!=0) {
+			return n1;
+		} else {
+			return n2;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter first number");
+		int n1 = scanner.nextInt();
+		
+		System.out.println("Enter first number");
+		int n2 = scanner.nextInt();
+		
+		int result = lameGCD(n1,n2);
+		System.out.println(result);
+	}
+
+}
