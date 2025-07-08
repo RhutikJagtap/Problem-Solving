824e03883587479efa456e7098118129c09464ec
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-08 11:31:50 +0530

    array insertion

---
+++ Array/src/day1/ArrayInsertion.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class ArrayInsertion {
+	
+	public static void main(String[] args) {
+		int[] arr =new int[5];
+		
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		arr[3]=40;
+		arr[4]=50;
+		
+	}
+
+}
