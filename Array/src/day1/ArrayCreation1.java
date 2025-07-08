661314ac93eb0cdc0b12180235c7bb17f9df8d43
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-08 11:28:31 +0530

    array creation

---
+++ Array/src/day1/ArrayCreation1.java
@@ -0,0 +1,40 @@
+package day1;
+
+public class ArrayCreation1 {
+	
+	public static void main(String[] args) {
+		
+		//array creation byte type
+		byte[] arr1=new byte[5];
+		
+		//array creation short type
+		short[] arr2=new short[5];
+		
+		//array creation integer type
+		int[] arr3 = new int[5];
+		
+		//array creation long type
+		long[] arr4 = new long[5];
+
+		//array creation float type
+		float[] arr5 = new float[5];
+		
+		//array creation double type
+		double[] arr6=new double[5];
+		
+		//array creation boolean type
+		boolean[] arr7 = new boolean[5];
+
+		//array creation char type
+		char[] arr8 = new char[5];
+		
+		//array creation String type
+		String[] arr9 = new String[5];
+		
+		//array creation Object type
+		Object arr10 = new Object[5];
+		
+		
+	}
+
+}
