39db84bc0fe4527fbcbf759171babdbde2eac33e
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-08 16:39:21 +0530

    pattern programs

---
+++ CodingBlock/src/pattern_programs/Pattern1.java
@@ -0,0 +1,20 @@
+package pattern_programs;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		
+		int i=1;
+		
+		while (i<=n) {
+			System.out.print("* ");
+			i++;
+		}
+		
+	}
+
+}
