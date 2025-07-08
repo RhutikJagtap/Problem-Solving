f480810307a6628510c6cf7fbb1ea4c9703d9786
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-08 11:37:16 +0530

    print the array elements

---
+++ Array/src/day1/PrintArray.java
@@ -0,0 +1,22 @@
+package day1;
+
+public class PrintArray {
+	
+	public static void main(String[] args) {
+		int[] arr =new int[5];
+		
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		arr[3]=40;
+		arr[4]=50;
+		
+		System.out.println("Array Elements");
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		
+	}
+
+}
