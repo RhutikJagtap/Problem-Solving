f33dd25a628e3488f5d1a4e83a1487b9f28dc3f4
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-08 19:21:52 +0530

    triangle star pattern

--- CodingBlock/src/pattern_programs/Pattern10.java
+++ CodingBlock/src/pattern_programs/Pattern10.java
@@ -3,34 +3,33 @@ package pattern_programs;
 import java.util.Scanner;
 
 public class Pattern10 {
-	
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Enter the value of n");
 		int n = scanner.nextInt();
-		
-		int row=1;
-		int k=1;
-		while (row<=n) {
-			
-			int spaces=1;
-			while(spaces<row)
-			{
+
+		int row = 1;
+		int k = 1;
+		while (row <= n) {
+
+			int spaces = 1;
+			while (spaces < row) {
 				System.out.print("  ");
 				spaces++;
 			}
-			
-			int column=1;
-			while (column<=(n*2)-k) {
+
+			int column = 1;
+			while (column <= (n * 2) - k) {
 				column++;
 				System.out.print("* ");
 			}
-			k=k+2;
+			k = k + 2;
 			row++;
 			System.out.println();
 		}
-		
+
 	}
 
 }
