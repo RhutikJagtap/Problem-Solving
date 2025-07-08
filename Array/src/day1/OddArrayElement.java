e4dc87b5b55ac39cbb707f3a95db0ec9733c5deb
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-08 12:08:42 +0530

    odd Array elements

---
+++ Array/src/day1/OddArrayElement.java
@@ -0,0 +1,27 @@
+package day1;
+
+import java.util.Scanner;
+
+public class OddArrayElement {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter the size of array");
+		int size = scanner.nextInt();
+
+		int[] arr = new int[size];
+
+		System.out.println("Enter the array Elements");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scanner.nextInt();
+		}
+
+		System.out.println("Odd Array element");
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] % 2 != 0) {
+				System.out.print(arr[i] + " ");
+			}
+		}
+	}
+
+}
