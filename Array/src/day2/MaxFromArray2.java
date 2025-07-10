d34add360100543a310a665da372fa2ee3625dd7
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-10 18:13:05 +0530

    find maximum from array

---
+++ Array/src/day2/MaxFromArray2.java
@@ -0,0 +1,35 @@
+package day2;
+
+import java.util.Scanner;
+
+public class MaxFromArray2 {
+
+	static int max(int[] arr) {
+		int max = Integer.MIN_VALUE;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		return max;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter the size of array ");
+		int size = scanner.nextInt();
+
+		int[] arr = new int[size];
+
+		System.out.println("Enter the array elements");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scanner.nextInt();
+		}
+
+		int result = max(arr);
+		System.out.println(result);
+	}
+
+}
--- Array/src/day2/package-info.java
+++ Array/src/day2/package-info.java
@@ -3,7 +3,8 @@ package day2;
 /*
  * 1.calculate the sum of array elements
  * 2.calculate the product of array elements
- * 
+ * 3.find maximum from array1
+ * 4.find maximum from array2
  *
  *
  *
