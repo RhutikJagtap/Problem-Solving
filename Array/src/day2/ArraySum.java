e18b4ae034457a7cc3191a575321679f478e4174
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-10 17:45:21 +0530

    calculate the array sum

---
+++ Array/src/day2/ArraySum.java
@@ -0,0 +1,34 @@
+package day2;
+
+import java.util.Scanner;
+
+public class ArraySum {
+
+	static int sum(int[] arr) {
+		int sum = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			sum = sum + arr[i];
+		}
+
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter the size of array");
+		int size = scanner.nextInt();
+
+		int[] arr = new int[size];
+
+		System.out.println("Enter the array elements");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scanner.nextInt();
+		}
+
+		int result = sum(arr);
+		System.out.println(result);
+	}
+
+}
---
+++ Array/src/day2/package-info.java
@@ -0,0 +1,11 @@
+package day2;
+
+/*
+ * 1.calculate the sum of array elements
+ * 
+ * 
+ *
+ *
+ *
+ *
+ */
