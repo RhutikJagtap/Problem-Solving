6eeec497d4cacc9b06f158d40485f3d7e6d8a874
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-11 10:02:41 +0530

    count how many times maximum number occur(optimized)

---
+++ Array/src/day3/CountMaxElement2.java
@@ -0,0 +1,39 @@
+package day3;
+
+import java.util.Scanner;
+
+//optimize
+public class CountMaxElement2 {
+
+	static int countMaxElement(int[] arr) {
+		int max = Integer.MIN_VALUE;
+
+		int maxCount = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+				maxCount = 1;
+			} else if (arr[i] == max) {
+				maxCount++;
+			}
+		}
+		return maxCount;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter the size of array");
+		int size = scanner.nextInt();
+
+		int[] arr = new int[size];
+
+		System.out.println("Enter the array elements");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scanner.nextInt();
+		}
+
+		int result = countMaxElement(arr);
+		System.out.println("Max Element count = " + result);
+	}
+}
--- Array/src/day3/package-info.java
+++ Array/src/day3/package-info.java
@@ -6,4 +6,5 @@ package day3;
  * 3.count the particular  element occur in array
  * 4.count how many times maximum number occur in array
  * 5.count how many times minimum number occur  in array
+ * 6.count how many times maximum number occur in array(Optimized)
 */
