35ddb57be82a9d3bd850f531042abeb56ed5de80
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-12 20:15:17 +0530

    second smallest element from array (without using sorting) variation 1

---
+++ Array/src/day4/SecondSmallestWithoutSorting1.java
@@ -0,0 +1,39 @@
+package day4;
+
+import java.util.Scanner;
+
+public class SecondSmallestWithoutSorting1 {
+
+	static int secondSmallest(int[] arr) {
+		int firstSmallest = arr[0], secondSmallest = Integer.MAX_VALUE;
+
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] < firstSmallest) {
+				secondSmallest = firstSmallest;
+				firstSmallest = arr[i];
+			} else if (arr[i] < secondSmallest) {
+				secondSmallest = arr[i];
+			}
+		}
+
+		return secondSmallest;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter the size of array");
+		int size = scanner.nextInt();
+
+		int[] arr = new int[size];
+
+		System.out.println("Enter the array elements");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scanner.nextInt();
+		}
+
+		int result = secondSmallest(arr);
+		System.out.println("Second Smallest " + result);
+	}
+
+}
--- Array/src/day4/package-info.java
+++ Array/src/day4/package-info.java
@@ -9,4 +9,8 @@ package day4;
  * 3.print the second largest element from array (without using sorting) variation 2
  * I/P  5 9 9 6 2      I/P  7 8 1 6  2
  * O/P   6               O/P  7
+ * 
+ *  4.print the second smallest element from array (without using sorting) variation 1
+ * I/P  10 9 2 6 2      I/P  7 8 1 6  2
+ * O/P    2               O/P  2
 */
