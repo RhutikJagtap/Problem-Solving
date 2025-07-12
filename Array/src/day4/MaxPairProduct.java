079cb005b7a989efc9a4d6a07bd5d8aa7269aec3
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-12 20:35:07 +0530

    print the max pair product from array

---
+++ Array/src/day4/MaxPairProduct.java
@@ -0,0 +1,38 @@
+package day4;
+
+import java.util.Scanner;
+
+public class MaxPairProduct {
+	
+	static int maxPairSum(int[] arr) {
+		int firstLargest = arr[0], secondLargest = Integer.MIN_VALUE;
+
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] > firstLargest) {
+				secondLargest = firstLargest;
+				firstLargest = arr[i];
+			} else if (arr[i] > secondLargest) {
+				secondLargest = arr[i];
+			}
+		}
+
+		return firstLargest*secondLargest;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter the size of array");
+		int size = scanner.nextInt();
+
+		int[] arr = new int[size];
+
+		System.out.println("Enter the array elements");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scanner.nextInt();
+		}
+
+		int result = maxPairSum(arr);
+		System.out.println("Max Pair Product =" + result);
+	}
+}
--- Array/src/day4/package-info.java
+++ Array/src/day4/package-info.java
@@ -25,4 +25,8 @@ package day4;
  * 7.print the min pair sum from array 
  * I/P   3 2 7 5 1 4 10 6
  * O/P   3
+ * 
+ * 8.print the max pair product from array 
+ * I/P   3 2 7 5 1 4 10 6
+ * O/P   70
 */
