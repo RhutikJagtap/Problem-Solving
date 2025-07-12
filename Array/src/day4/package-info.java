2621f0f3a1686ddd2157adf89a1df3e8a0e771c3
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-12 19:44:20 +0530

    second largest element from array (without using sorting) variation 1

--- Array/src/day4/package-info.java
+++ Array/src/day4/package-info.java
@@ -2,4 +2,7 @@ package day4;
 
 /*
  * 1.print the second largest element from array (using sorting)
+ * 2.print the second largest element from array (without using sorting) variation 1
+ * I/P  10 9 9 6 2      I/P  7 8 1 6  2
+ * O/P   9                O/P  7
 */
