9bf8667a416f482308c3b61d6b2aa86a565e8393
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-13 11:28:42 +0530

    search the particular element in array return last occurence

---
+++ Array/src/day5/LastIndex.java
@@ -0,0 +1,38 @@
+package day5;
+
+import java.util.Scanner;
+
+public class LastIndex {
+
+	static int search(int[] arr, int target) {
+		for (int i = arr.length - 1; i >= 0; i--) {
+			if (arr[i] == target) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter the size of array");
+		int size = scanner.nextInt();
+
+		int[] arr = new int[size];
+
+		System.out.println("Enter the array elements");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scanner.nextInt();
+		}
+
+		System.out.println("Enter the target element");
+		int target = scanner.nextInt();
+
+		int result = search(arr, target);
+		System.out.println("Target element found at index = "+result);
+
+	}
+
+}
--- Array/src/day5/package-info.java
+++ Array/src/day5/package-info.java
@@ -2,4 +2,5 @@ package day5;
 
 /*
  * 1.search the particular element in array if found return  index. if not found return -1
+ * 2.search the particular element in array return last occurence
  */
