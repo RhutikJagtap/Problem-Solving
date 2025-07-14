36842044575991b8d8397ae38b8868ccf45e3535
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-14 19:37:40 +0530

    added comments

--- PWSkills/src/array/PrintUniqueElementFromUnsortedArray.java
+++ PWSkills/src/array/PrintUniqueElementFromUnsortedArray.java
@@ -7,12 +7,14 @@ public class PrintUniqueElementFromUnsortedArray {
 		for (int i = 0; i < arr.length-1; i++) {
 			for (int j = i+1; j < arr.length; j++) {
 				if (arr[i]==arr[j]) {
+					//if element matches means duplicate i will mark it as -1
 					arr[i]=-1;
 					arr[j]=-1;
 				}
 			}
 		}
 		
+		//the elements which are not equal to -1 are unique elements
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i]!=-1) {
 				return arr[i];
