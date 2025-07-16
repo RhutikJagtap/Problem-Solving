a0cad200cd3e456459a142e36bdc00d15fc4be48
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-16 17:09:49 +0530

    * 6.given an array of size N,print all the subarrays of all sizes

---
+++ Array/src/day8/PrintAllSubArrayInReverse.java
@@ -0,0 +1,40 @@
+package day8;
+
+import java.util.Scanner;
+
+//print all the subarrays of all sizes in reverse
+public class PrintAllSubArrayInReverse {
+
+	static void printSubArrayOfSize(int[] arr, int size) {
+		for (int i = 0; i <= arr.length - size; i++) {
+			for (int j = i; j < size + i; j++) {
+				System.out.print(arr[j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	static void printAllSubArray(int[] arr) {
+		for (int size = arr.length; size >= 1; size--) {
+			printSubArrayOfSize(arr, size);
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter the size of array");
+		int n = scanner.nextInt();
+
+		int[] arr = new int[n];
+
+		System.out.println("Enter the array elements");
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scanner.nextInt();
+		}
+
+		System.out.println("All Size SubArrays");
+		printAllSubArray(arr);
+	}
+
+}
--- Array/src/day8/package-info.java
+++ Array/src/day8/package-info.java
@@ -42,4 +42,8 @@ package day8;
  * 6.given an array of size N,print all the subarrays of all sizes
  * I/p 9
  *     5  1 2 4 9 3 1 3 3
+ *     
+ * 7.given an array of size N,print all the subarrays of all sizes in reverse
+ * I/p 9
+ *     5  1 2 4 9 3 1 3 3    
  */
