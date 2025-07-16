f5ecb45458324b55005348346af6c1aaf89aff51
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-16 17:31:23 +0530

    Array search

---
+++ GeeksForGeeks/src/array/basic/ArraySearch.java
@@ -0,0 +1,17 @@
+package array.basic;
+
+//Given an array,arr of n integers,and an integer element x,find whether element x is present in the array.
+//Return the index of the first occurrence of x in the array,or-1 if it doesn't exist.
+public class ArraySearch {
+
+	static int search(int arr[], int x) {
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == x) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+}
