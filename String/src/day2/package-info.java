ef330844290275e33556942f5a53ebac20204a11
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-19 18:59:55 +0530

    count words in the string

--- String/src/day2/package-info.java
+++ String/src/day2/package-info.java
@@ -4,4 +4,9 @@ package day2;
  * 1.write a program to count spaces in the string
  * I/p how are you 
  * O/p 2
+ * 
+ * 2.write a program to count total words in the string
+  * I/p how are you 
+ * O/p 3
+ * 
  */
