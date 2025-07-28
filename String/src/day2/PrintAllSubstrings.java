1b5444dfb9df593bbe86c31548d1c95defda2e2a
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-28 17:10:45 +0530

    print all substrings

---
+++ String/src/day2/PrintAllSubstrings.java
@@ -0,0 +1,30 @@
+package day2;
+
+import java.util.Scanner;
+
+public class PrintAllSubstrings {
+
+	static void printAllSubstring(String str) {
+		for (int size = 1; size <= str.length(); size++) {
+
+			for (int i = 0; i <= str.length() - size; i++) {
+				String t = "";
+				for (int j = i; j < size + i; j++) {
+					t = t + str.charAt(j);
+				}
+				System.out.println(t);
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter the string");
+		String str = scanner.nextLine();
+
+		printAllSubstring(str);
+	}
+
+}
