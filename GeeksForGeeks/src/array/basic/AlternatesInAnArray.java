931979061ccaa54af54327eb43672bc27c33e3b3
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-29 09:36:04 +0530

    print alternates element in array

---
+++ GeeksForGeeks/src/array/basic/AlternatesInAnArray.java
@@ -0,0 +1,43 @@
+package array.basic;
+
+import java.util.ArrayList;
+
+/*You are given an array arr[], the task is to return a list elements of arr in alternate order (starting from index 0).
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 4]
+Output: 1 3
+Explanation:
+Take first element: 1
+Skip second element: 2
+Take third element: 3
+Skip fourth element: 4
+
+Input: arr[] = [1, 2, 3, 4, 5]
+Output: 1 3 5
+Explanation:
+Take first element: 1
+Skip second element: 2
+Take third element: 3
+Skip fourth element: 4
+Take fifth element: 5
+*/
+public class AlternatesInAnArray {
+
+	public static ArrayList<Integer> getAlternates(int arr[]) {
+		ArrayList list = new ArrayList();
+		for (int i = 0; i < arr.length; i += 2) {
+			list.add(arr[i]);
+		}
+
+		return list;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5 };
+		ArrayList<Integer> res = getAlternates(arr);
+		System.out.println(res);
+	}
+
+}
