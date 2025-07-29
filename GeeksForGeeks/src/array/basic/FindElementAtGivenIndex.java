3477af73fcac9f64b082b54d8a5cfce1c620d3e8
RhutikJagtap <rhutikjagtap68@gmail.com>
2025-07-29 23:52:36 +0530

    find element at given index

---
+++ GeeksForGeeks/src/array/basic/FindElementAtGivenIndex.java
@@ -0,0 +1,28 @@
+package array.basic;
+
+/*Given an array arr of integers and an index key(0-based index). 
+ * Your task is to return the element present at the index key in the array.
+
+Examples:
+
+Input: key = 2 , arr = [10, 20, 30, 40, 50]
+Output: 30
+Explanation: The value of arr[2] is 30 .
+
+Input: key = 4 , arr = [10, 20, 30, 40, 50, 60, 70]
+Output: 50
+Explanation: The value of the arr[4] is 50 .*/
+public class FindElementAtGivenIndex {
+	public static int findElementAtIndex(int key, int[] arr) {
+		return arr[key];
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 10, 20, 30, 40, 50, 60, 70 };
+		int key = 4;
+
+		int result = findElementAtIndex(key, arr);
+		System.out.println(result);
+	}
+
+}
